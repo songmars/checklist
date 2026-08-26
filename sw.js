89d1cccfcad67cb97c231688dab0c34e57f1bf3f
songmars <25-10808@inje.icehs.kr>
2026-08-26 15:55:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', function(e) {});
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    // PWA 필수 서비스 워커 기본 동작
+});
